5a18d3383e35c0d212fea0a87f426892810cf4fc
Tommy Connor <tconnor98@aol.com>
2020-02-12 18:57:18 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,7 +5,7 @@ class Artist
 
   def initialize(name)
     @name = name
-    
+    @@song_count += 1
   end
   
   def songs
@@ -14,6 +14,7 @@ class Artist
 
   def add_song(song)
     song.artist = self
+    @@song_count += 1 
   end
 
   def add_song_by_name(name)
